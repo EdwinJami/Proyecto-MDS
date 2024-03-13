7259cf72a0bac1bcbcc314fcdc10711d5fab9aeb
Anahy Herrera <117839629+Any090802@users.noreply.github.com>
2024-03-12 19:36:38 -0500

    caledar
    calendario

--- Principal.py
+++ Principal.py
@@ -1,13 +1,31 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import PhotoImage
-
+from tkinter import PhotoImage
+import sys
+sys.path.append('/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-packages')
+from tkcalendar import Calendar
 
 root = Tk()
 root.title('Login')
 root.geometry('925x500+300+200')
 root.configure(bg="#fff")
 root.resizable(False, False)
+
+# Encabezado
+header = Label(root, text='Gestión de eventos escolares de la Unidad Educativa "Lev Vygotsky"', bg='#fff', fg='#333', font=('Arial', 20, 'bold'))
+header.pack(pady=20, padx=20)
+
+# Resto del código sin cambios
+img = PhotoImage(file='login.png')
+label_img = Label(root, image=img, bg='white')
+label_img.place(x=50, y=50)
+
+frame = Frame(root, width=350, height=350, bg="white")
+frame.place(x=480, y=70)
+
+heading = Label(frame, text='Sign in', fg='#57a1f8', bg='white', font=('Microsoft YaHei UI Light', 23, 'bold'))
+heading.place(x=100, y=5)
 
 def signin():
     username=user.get()
@@ -19,9 +37,10 @@ def signin():
         screen.geometry('925x500+300+200')
         screen.config(bg="white")
 
-        Label(screen, text='¡Hola a todos!', bg='#fff', font=('Calibri', 50, 'bold')).pack(expand=True)
-        screen.mainloop()
+        cal = Calendar(screen, selectmode='day', year=2024, month=3, day=13)
+        cal.pack(pady=20)
 
+        screen.mainloop()
 
     elif username != 'admin' and password != '1234':
         messagebox.showerror("Inválido", "nombre de usuario y contraseña inválidos")
@@ -81,8 +100,4 @@ label.place(x=75, y=270)
 sign_up = Button(frame, width=6, text='Sign up', border=0, bg='white', cursor='hand2', fg='#57a1f8')
 sign_up.place(x=215, y=270)
 
-
-
 root.mainloop()
-
-#probando los comit
